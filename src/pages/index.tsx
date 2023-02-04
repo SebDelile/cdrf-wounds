981935183fdee7c319b5e52428e573bfd707a0d9
Delile Sebastien <sebastiendelile@gmail.com>
2023-02-04 18:45:14 +0100

    add style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import { inputsType } from '@/types/inputsType';
 import { outputsType } from '@/types/outputsType';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { computeResults } from '@/utils/computeResults';
-import { woundResults } from 'src/constants/woundResults';
-import { woundTable } from '@/constants/woundTable';
 import { initialResults } from '@/constants/initialResults';
+import { Box } from '@mui/system';
 
 export default function Home() {
   const [isDebug, setIsDebug] = useState(false);
@@ -23,7 +33,9 @@ export default function Home() {
   const [outputs, setOutputs] = useState<outputsType>(initialResults);
 
   useEffect(() => {
-    setOutputs(computeResults(inputs, isDebug));
+    if (Number.isInteger(inputs.FOR) && Number.isInteger(inputs.RES)) {
+      setOutputs(computeResults(inputs, isDebug));
+    }
   }, [inputs, isDebug]);
 
   const handleToggleBoolean = (field: keyof inputsType) => {
@@ -39,120 +51,179 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
       </Head>
-      <main>
-        <div>
-          <label>
-            {'FOR : '}
-            <input
-              type="number"
-              value={inputs.FOR}
-              onChange={(event) => {
-                setInputs((state) => ({
-                  ...state,
-                  FOR: parseInt(event.target.value),
-                }));
-              }}
-            />
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={inputs.armeSacree}
-              onChange={() => {
-                handleToggleBoolean('armeSacree');
-              }}
-            />
-            {'Arme sacrée'}
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={inputs.tirImmobile}
-              onChange={() => {
-                handleToggleBoolean('tirImmobile');
-              }}
-            />
-            {'Tir immobile'}
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={inputs.fleau}
-              onChange={() => {
-                handleToggleBoolean('fleau');
-              }}
-            />
-            {'Fleau'}
-          </label>
-        </div>
-        <div>
-          <label>
-            {'RES : '}
-            <input
-              type="number"
-              value={inputs.RES}
-              onChange={(event) => {
-                setInputs((state) => ({
-                  ...state,
-                  RES: parseInt(event.target.value),
-                }));
-              }}
-            />
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={inputs.armureSacree}
-              onChange={() => {
-                handleToggleBoolean('armureSacree');
-              }}
-            />
-            {'Armure sacrée'}
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={inputs.durACuire}
-              onChange={() => {
-                handleToggleBoolean('durACuire');
-              }}
-            />
-            {'Dur à cuire'}
-          </label>
-        </div>
-        <table>
-          <thead>
-            <tr>
-              <th>{`(${totalOutputs})`}</th>
-              <th>Somme</th>
-              <th>Proportion</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.entries(outputs).map(([resultat, somme]) => (
-              <tr key={resultat}>
-                <th>{resultat}</th>
-                <td>{somme}</td>
-                <td>
-                  {totalOutputs ? Math.round((somme / totalOutputs) * 100) : 0}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </main>
-      <footer>
-        <label>
-          <input
-            type="checkbox"
-            checked={isDebug}
-            onChange={() => {
-              setIsDebug((state) => !state);
-            }}
+      <Grid
+        container
+        spacing={2}
+        component={'main'}
+        sx={{
+          padding: '2rem',
+        }}
+      >
+        <Grid container item xs={12} md={6} spacing={2}>
+          <Grid item xs={6}>
+            <Grid
+              container
+              direction="column"
+              component={Paper}
+              sx={{ padding: '1rem' }}
+            >
+              <Grid item>
+                <TextField
+                  id="input-for"
+                  label="FOR"
+                  type="number"
+                  value={inputs.FOR}
+                  onChange={(event) => {
+                    setInputs((state) => ({
+                      ...state,
+                      FOR: parseInt(event.target.value),
+                    }));
+                  }}
+                  error={!Number.isInteger(inputs.FOR)}
+                  helperText={
+                    !Number.isInteger(inputs.FOR) ? 'FOR est obligatoire' : null
+                  }
+                />
+              </Grid>
+              <Grid item>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={inputs.armeSacree}
+                      onChange={() => {
+                        handleToggleBoolean('armeSacree');
+                      }}
+                    />
+                  }
+                  label="Arme sacrée"
+                />
+              </Grid>
+              <Grid item>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={inputs.tirImmobile}
+                      onChange={() => {
+                        handleToggleBoolean('tirImmobile');
+                      }}
+                    />
+                  }
+                  label="Tir immobile"
+                />
+              </Grid>
+              <Grid item>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={inputs.fleau}
+                      onChange={() => {
+                        handleToggleBoolean('fleau');
+                      }}
+                    />
+                  }
+                  label="Fleau"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={6}>
+            <Grid
+              container
+              direction="column"
+              component={Paper}
+              sx={{ padding: '1rem' }}
+            >
+              <Grid item>
+                <TextField
+                  id="input-res"
+                  label="RES"
+                  type="number"
+                  value={inputs.RES}
+                  onChange={(event) => {
+                    setInputs((state) => ({
+                      ...state,
+                      RES: parseInt(event.target.value),
+                    }));
+                  }}
+                  error={!Number.isInteger(inputs.RES)}
+                  helperText={
+                    !Number.isInteger(inputs.RES) ? 'RES est obligatoire' : null
+                  }
+                />
+              </Grid>
+              <Grid item>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={inputs.armureSacree}
+                      onChange={() => {
+                        handleToggleBoolean('armureSacree');
+                      }}
+                    />
+                  }
+                  label="Armure sacrée"
+                />
+              </Grid>
+              <Grid item>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={inputs.durACuire}
+                      onChange={() => {
+                        handleToggleBoolean('durACuire');
+                      }}
+                    />
+                  }
+                  label="Dur à cuire"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>{`(${totalOutputs})`}</TableCell>
+                  <TableCell>Somme</TableCell>
+                  <TableCell>Proportion</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Object.entries(outputs).map(([resultat, somme]) => (
+                  <TableRow key={resultat}>
+                    <TableCell>{resultat}</TableCell>
+                    <TableCell>{somme}</TableCell>
+                    <TableCell>
+                      {totalOutputs
+                        ? `${parseFloat(
+                            ((somme / totalOutputs) * 100).toFixed(1)
+                          )} %`
+                        : '-'}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Grid>
+      <Box component="footer" sx={{ padding: '2rem' }}>
+        <Paper sx={{ padding: '1rem' }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={isDebug}
+                onChange={() => {
+                  setIsDebug((state) => !state);
+                }}
+              />
+            }
+            label="Debug"
           />
-          {'Debug'}
-        </label>
-      </footer>
+        </Paper>
+      </Box>
     </>
   );
 }
