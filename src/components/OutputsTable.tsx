512a7bc21e071b1cc391ee9425c983073001d111
Delile Sebastien <sebastiendelile@gmail.com>
2023-02-11 11:09:53 +0100

    add column into OutputsTable

--- src/components/OutputsTable.tsx
+++ src/components/OutputsTable.tsx
@@ -4,7 +4,6 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import { outputsType } from '@/types/outputsType';
 import { woundResultsLabels } from '@/constants/woundResults';
 
@@ -14,28 +13,43 @@ type propTypes = {
 
 export default function OutputsTable({ outputs }: propTypes) {
   const totalOutputs = outputs.reduce((a, b) => a + b, 0);
+  // for each : [count, percentage, percentage min]
+  const displayedOutputs = outputs.map((output, index) => [
+    parseFloat(output.toFixed(2)),
+    `${parseFloat(((output / totalOutputs) * 100).toFixed(1))} %`,
+    index
+      ? `${parseFloat(
+          (
+            (outputs.reduce((a, b, j) => a + (index <= j ? b : 0), 0) /
+              totalOutputs) *
+            100
+          ).toFixed(1)
+        )} %`
+      : '-',
+  ]);
+
   return (
-    <TableContainer component={Paper}>
+    <TableContainer>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>{`(${totalOutputs})`}</TableCell>
             <TableCell>Somme</TableCell>
             <TableCell>Proportion</TableCell>
+            <TableCell>Proportion résultat minimal</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {outputs.map((count, i) => (
-            <TableRow key={i}>
-              <TableCell>{woundResultsLabels[i]}</TableCell>
-              <TableCell>{parseFloat(count.toFixed(2))}</TableCell>
-              <TableCell>
-                {totalOutputs
-                  ? `${parseFloat(((count / totalOutputs) * 100).toFixed(1))} %`
-                  : '-'}
-              </TableCell>
-            </TableRow>
-          ))}
+          {totalOutputs
+            ? displayedOutputs.map((displayedOutput, i) => (
+                <TableRow key={i}>
+                  <TableCell>{woundResultsLabels[i]}</TableCell>
+                  <TableCell>{displayedOutput[0]}</TableCell>
+                  <TableCell>{displayedOutput[1]}</TableCell>
+                  <TableCell>{displayedOutput[2]}</TableCell>
+                </TableRow>
+              ))
+            : null}
         </TableBody>
       </Table>
     </TableContainer>
